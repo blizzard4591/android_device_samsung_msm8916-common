d691604415799538b46f20bd24cca9b277a14d86
Berger <berger@cs.rwth-aachen.de>
2021-04-21 21:41:10 +0200

    Camera to camera.vendor

--- msm8916.mk
+++ msm8916.mk
@@ -116,6 +116,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libcamera_shim \
     libmm-qcamera \
