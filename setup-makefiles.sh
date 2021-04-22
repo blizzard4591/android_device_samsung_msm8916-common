a64404e747eb25b5875fcafd92eb05f09848dab9
Berger <berger@cs.rwth-aachen.de>
2021-04-22 12:35:57 +0200

    Revert setup-makefiles.sh back to the 17.1 version (that works).

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2017 The LineageOS Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 set -e
+
+BOARD_COMMON=msm8916-common
 
 DEVICES_A3="a3lte a33g a3ulte"
 DEVICES_A5="a5ltechn a5ltectc"
@@ -23,59 +35,78 @@ DEVICES_SERRANO="serranovelte serranove3g"
 DEVICES_ALL="$DEVICES_A3 $DEVICES_A5 $DEVICES_GPRIME $DEVICES_GTE $DEVICES_J3 $DEVICES_J3XPRO \
 	$DEVICES_J5 $DEVICES_J5X $DEVICES_J7 $DEVICES_O7 $DEVICES_SERRANO"
 
+VENDOR=samsung
+
+INITIAL_COPYRIGHT_YEAR=2017
+
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-ANDROID_ROOT="${MY_DIR}/../../.."
+LINEAGE_ROOT="$MY_DIR"/../../..
+DEVICE_DIR="$MY_DIR"/../$DEVICE
+DEVICE_COMMON_DIR="$MY_DIR"/../$DEVICE_COMMON
 
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
+# determine which blob dirs to set up
+if [ -z "$SETUP_BOARD_COMMON_DIR" ]; then
+    SETUP_BOARD_COMMON_DIR=1
+fi
+
+if [ -z "$SETUP_DEVICE_DIR" ]; then
+    SETUP_DEVICE_DIR=0
+fi
+
+if [ -z "$SETUP_DEVICE_COMMON_DIR" ]; then
+    SETUP_DEVICE_COMMON_DIR=0
+fi
+
+HELPER="$LINEAGE_ROOT"/tools/extract-utils/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
     exit 1
 fi
-source "${HELPER}"
+. "$HELPER"
 
-# Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
+if [ "$SETUP_DEVICE_COMMON_DIR" -eq 1 ] && [ -s $DEVICE_COMMON_DIR/proprietary-files.txt ]; then
+    # Reinitialize the helper for device
+    setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
-# Warning headers and guards
-write_headers "${DEVICES_ALL}"
+    # Copyright headers and guards
+    write_headers "$DEVICES"
 
-# The standard common blobs
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
+    # The standard device blobs
+    write_makefiles $DEVICE_COMMON_DIR/proprietary-files.txt
 
-# Finish
-write_footers
-
-if [ -s "${MY_DIR}/../${DEVICE_SPECIFIED_COMMON}/proprietary-files.txt" ]; then
-    DEVICE_COMMON="${DEVICE_SPECIFIED_COMMON}"
-
-    # Reinitialize the helper for device specified common
-    setup_vendor "${DEVICE_SPECIFIED_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
-
-    # Warning headers and guards
-    write_headers "${DEVICE_SPECIFIED_COMMON_DEVICE}"
-
-    # The standard device specified common blobs
-    write_makefiles "${MY_DIR}/../${DEVICE_SPECIFIED_COMMON}/proprietary-files.txt" true
-
-    # Finish
+    # We are done!
     write_footers
-
-    DEVICE_COMMON="msm8916-common"
 fi
 
-if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
+if [ "$SETUP_DEVICE_DIR" -eq 1 ] && [ -s $DEVICE_DIR/proprietary-files.txt ]; then
     # Reinitialize the helper for device
-    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
+    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
 
-    # Warning headers and guards
+    # Copyright headers and guards
     write_headers
 
     # The standard device blobs
-    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
+    write_makefiles $DEVICE_DIR/proprietary-files.txt
 
-    # Finish
+    # We are done!
     write_footers
+fi
+
+if  [ "$SETUP_BOARD_COMMON_DIR" -eq 1 ]; then
+   # set up the board common makefiles
+   DEVICE_COMMON=$BOARD_COMMON
+
+   # Initialize the helper
+   setup_vendor "$BOARD_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
+
+   # Copyright headers and guards
+   write_headers "$DEVICES_ALL"
+
+   write_makefiles "$MY_DIR"/proprietary-files.txt
+
+   # Finish
+   write_footers
 fi
