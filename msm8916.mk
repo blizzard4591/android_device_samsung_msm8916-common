39de3eecec83d23d757208e5a1fda8d9c28da40e
Berger <berger@cs.rwth-aachen.de>
2021-04-22 13:36:49 +0200

    Revert useless changes I did while working on the camera.

--- msm8916.mk
+++ msm8916.mk
@@ -69,11 +69,11 @@ PRODUCT_COPY_FILES += \
 #    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 #endif
 
-# Audio encoders
-PRODUCT_PROPERTY_OVERRIDES += \
-    qcom.hw.aac.encoder=false
-
 # Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
+
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
 
@@ -81,47 +81,15 @@ PRODUCT_PACKAGES += \
     libbase_shim \
     libbt-vendor
 
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service \
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    bluetooth.hfp.client=1 \
-    ro.bluetooth.dun=true \
-    ro.bluetooth.hfp.ver=1.7 \
-    ro.bluetooth.sap=true \
-    ro.qualcomm.bt.hci_transport=smd \
-    vendor.bluetooth.soc=pronto \
-    vendor.qcom.bluetooth.soc=pronto
-
-
-
-# BoringSSL Hacks
-PRODUCT_PACKAGES += \
-    libboringssl-compat
-
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
-# Camera
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    camera2.portability.force_api=1
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    camera.disable_treble=true \
-    camera2.portability.force_api=1 \
-    debug.camcorder.disablemeta=true
 
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-	libbson \
     libcamera_shim \
-    libmm-qcamera \
     camera.msm8916 \
-    libboringssl-compat \
     Snap
 
 # Connectivity Engine support
@@ -133,11 +101,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/dsi_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/dsi_config.xml \
     $(LOCAL_PATH)/configs/data/netmgr_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/netmgr_config.xml \
     $(LOCAL_PATH)/configs/data/qmi_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/qmi_config.xml
-
-# Data modules
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.data.netmgrd.qos.enable=false \
-    ro.use_data_netmgrd=false
 
 # Dex
 PRODUCT_DEXPREOPT_SPEED_APPS += \
@@ -155,7 +118,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     gralloc.msm8916 \
     hwcomposer.msm8916 \
-    libgenlock \
     libtinyxml \
     libtinyxml2 \
     memtrack.msm8916 \
@@ -185,17 +147,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fsck.f2fs
 
-# Graphics
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.enable_gl_backpressure=1 \
-    debug.composition.type=c2d \
-    debug.egl.hw=1 \
-    debug.sf.hw=1 \
-    debug.hwui.use_buffer_age=false
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
-
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service.software
@@ -216,17 +167,6 @@ PRODUCT_PACKAGES += \
     gps.msm8916 \
     libshim_gps \
     liblocadapterbase_shim
-
-# GPS Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.gps.qc_nlp_in_use=1 \
-    persist.loc.nlp_name=com.qualcomm.location \
-    ro.gps.agps_provider=1 \
-    ro.pip.gated=0
-
-# Headers
-PRODUCT_VENDOR_KERNEL_HEADERS := \
-    hardware/qcom/msm8916/kernel-headers
 
 # Health HAL
 PRODUCT_PACKAGES += \
@@ -292,45 +232,15 @@ PRODUCT_PACKAGES += \
     libOmxCore \
     libOmxEvrcEnc \
     libOmxQcelp13Enc \
-    libOmxSwVencMpeg4 \
     libOmxVdec \
-    libOmxVdecHevc \
-    libOmxVenc \
-    libOmxVidEnc \
-    libOmxVdpp
-
-# Media
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.stagefright.omx_default_rank.sw-audio=1 \
-    debug.stagefright.omx_default_rank=0 \
-    persist.media.treble_omx=false \
-    media.aac_51_output_enabled=true \
-    media.stagefright.enable-aac=true \
-    media.stagefright.enable-fma2dp=true \
-    media.stagefright.enable-http=true \
-    media.stagefright.enable-player=true \
-    media.stagefright.enable-qcp=true \
-    media.stagefright.enable-scan=true \
-    media.stagefright.legacyencoder=true \
-    media.stagefright.less-secure=true \
-    media.stagefright.use-awesome=true \
-    media.swhevccodectype=0 \
-    debug.stagefright.ccodec=0 \
-    mm.enable.qcom_parser=3183219 \
-    mm.enable.smoothstreaming=true \
-    mmp.enable.3g2=true
+    libOmxVenc
 
 # Misc
 PRODUCT_PACKAGES += \
     curl \
-    libbson \
     libcurl \
     libkeyutils \
     tcpdump
-
-# Perf
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=libqti-perfd-client.so
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -375,11 +285,6 @@ PRODUCT_PACKAGES += \
     libxml2 \
     macloader
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.add_power_save=1 \
-    persist.radio.apm_sim_not_pwdn=1 \
-    persist.radio.sib16_support=1
-
 # Rootdir
 PRODUCT_PACKAGES += \
     init.class_main.sh \
@@ -412,13 +317,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
-
-# SAMP SPCM
-PRODUCT_PROPERTY_OVERRIDES += \
-    sys.config.samp_spcm_enable=true \
-    sys.config.spcm_db_enable=true \
-    sys.config.spcm_db_launcher=true \
-    sys.config.spcm_preload_enable=true
 
 # Security configuration file
 PRODUCT_COPY_FILES += \
@@ -465,10 +363,8 @@ PRODUCT_PACKAGES += \
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service.legacy \
-    android.hardware.wifi.offload@1.0-service \
     hostapd \
     hostapd_cli \
-    iwconfig \
     libwpa_client \
     libwcnss_qmi \
     wcnss_service \
