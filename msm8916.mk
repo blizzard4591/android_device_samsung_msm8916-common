81a37f363022ac68f6cb36859663d6b03bf0e2c3
Berger <berger@cs.rwth-aachen.de>
2021-04-24 15:22:10 +0200

    Still on RIL

--- msm8916.mk
+++ msm8916.mk
@@ -287,7 +287,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.0-impl \
-    android.hardware.radio.service
+    android.hardware.radio@1.0-service \
+    android.hardware.radio@1.2-impl \
+    android.hardware.radio@1.2-service \
+    android.hardware.radio.config@1.0-service
 
 # Rootdir
 PRODUCT_PACKAGES += \
